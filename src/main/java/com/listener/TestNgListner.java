44a6525cd4dd5e779430bb4563217694234590c1
gcp111093 <gcp111093@gmail.com>
2020-05-17 19:50:24 +0000

    Updated VInod

--- src/main/java/com/listener/TestNgListner.java
+++ src/main/java/com/listener/TestNgListner.java
@@ -15,43 +15,35 @@ public class TestNgListner  implements ITestListener {
     //Extent Report Declarations
     private static ExtentReports extent = ExtentManager.createInstance();
     private static ThreadLocal<ExtentTest> test = new ThreadLocal<ExtentTest>();
-    
-	@Override
 	public void onTestStart(ITestResult result) {
 		// TODO Auto-generated method stub
 		
 	}
-	@Override
 	public void onTestSuccess(ITestResult result) {
 		// TODO Auto-generated method stub
 		
 	}
-	@Override
 	public void onTestFailure(ITestResult result) {
 		// TODO Auto-generated method stub
 		
 	}
-	@Override
 	public void onTestSkipped(ITestResult result) {
 		// TODO Auto-generated method stub
 		
 	}
-	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		// TODO Auto-generated method stub
 		
 	}
-	@Override
 	public void onStart(ITestContext context) {
 		// TODO Auto-generated method stub
 		
 	}
-	@Override
 	public void onFinish(ITestContext context) {
 		// TODO Auto-generated method stub
 		
 	}
-
+    
  
 
    
